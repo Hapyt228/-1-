12667fb58a4bad1c843d7e3914a6e6a7d7428ae1
Hapyt228 <skvitcnheko@gmail.com>
2025-05-20 15:59:12 +0300

    Да

--- project.cpp
+++ project.cpp
@@ -24,6 +24,7 @@ int main() {
             break;
         case 2:
             registerUser();
+            Cleaning();
             break;
         case 3:
             cout << "Выход из программы." << endl;
